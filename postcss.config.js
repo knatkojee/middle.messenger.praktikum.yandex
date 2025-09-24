874c06087bc353449fd7684325b61b3386f1db9b
Eгор Kалмыков <ekalmykov@studio-tg.ru>
2025-09-24 15:51:29 +0300

    init commit

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+export default {
+  plugins: [
+    require("postcss-preset-env")({
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+        "custom-media-queries": true,
+        "media-query-ranges": true,
+      },
+    }),
+  ],
+};
