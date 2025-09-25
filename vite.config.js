e7a760ed681ee20bb459136657f399f524ba9e74
Eгор Kалмыков <knatkojee@yandex.ru>
2025-09-25 17:54:50 +0300

    vite config upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
-  publicDir: 'static',
+  publicDir: "static",
+  build: {
+    assetsDir: "source",
+    outDir: "build",
+    cssMinify: false,
+    minify: false,
+  },
   css: {
     postcss: "./postcss.config.js",
   },
+  plugins: [handlebars()],
 });
