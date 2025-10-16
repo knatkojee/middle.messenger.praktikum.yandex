e5bf1831cde914c8c87d1b433f8afe87c092f415
Eгор Kалмыков <knatkojee@yandex.ru>
2025-10-16 20:44:52 +0300

    Линтер

---
+++ eslint.config.ts
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'build/**',
+      '.vite/**',
+      'coverage/**',
+      'public/**',
+      '**/*.hbs',
+      'src/main.ts',
+      'vite.config.ts'
+    ]
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    }
+  }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Handlebars from "handlebars";
-import * as Pages from "./pages";
+
 import * as Components from "./components";
+import * as Pages from "./pages";
 
 const pages = {
   login: [
