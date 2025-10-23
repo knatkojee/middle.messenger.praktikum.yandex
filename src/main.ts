382f706d82f3136ed106717022dffba6471ff23a
Eгор Kалмыков <knatkojee@yandex.ru>
2025-10-23 21:14:21 +0300

    main page layout

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Handlebars from "handlebars";
+import Handlebars from 'handlebars';
 
-import * as Components from "./components";
-import * as Pages from "./pages";
+import * as Components from './components';
+import * as Pages from './pages';
 
 const pages = {
   login: [
@@ -9,16 +9,16 @@ const pages = {
     {
       fields: [
         {
-          label: "Логин",
-          inputType: "text",
-          inputValue: "ivanivanov",
-          name: "login",
+          label: 'Логин',
+          inputType: 'text',
+          inputValue: 'ivanivanov',
+          name: 'login',
         },
         {
-          label: "Пароль",
-          inputType: "password",
-          inputValue: "password",
-          name: "password",
+          label: 'Пароль',
+          inputType: 'password',
+          inputValue: 'password',
+          name: 'password',
         },
       ],
     },
@@ -28,50 +28,50 @@ const pages = {
     {
       fields: [
         {
-          label: "Почта",
-          inputType: "email",
-          inputValue: "pochta@yandex.ru",
-          name: "email",
+          label: 'Почта',
+          inputType: 'email',
+          inputValue: 'pochta@yandex.ru',
+          name: 'email',
         },
         {
-          label: "Логин",
-          inputType: "text",
-          inputValue: "ivanivanov",
-          name: "login",
+          label: 'Логин',
+          inputType: 'text',
+          inputValue: 'ivanivanov',
+          name: 'login',
         },
         {
-          label: "Имя",
-          inputType: "text",
-          inputValue: "Иван",
-          name: "first_name",
+          label: 'Имя',
+          inputType: 'text',
+          inputValue: 'Иван',
+          name: 'first_name',
         },
         {
-          label: "Фамилия",
-          inputType: "text",
-          inputValue: "Иванов",
-          name: "second_name",
+          label: 'Фамилия',
+          inputType: 'text',
+          inputValue: 'Иванов',
+          name: 'second_name',
         },
         {
-          label: "Телефон",
-          inputType: "tel",
-          inputValue: "+7 (909) 967 30 30",
-          name: "phone",
+          label: 'Телефон',
+          inputType: 'tel',
+          inputValue: '+7 (909) 967 30 30',
+          name: 'phone',
         },
         {
-          label: "Пароль",
-          inputType: "password",
-          inputValue: "password",
+          label: 'Пароль',
+          inputType: 'password',
+          inputValue: 'password',
           invalid: true,
-          name: "password",
+          name: 'password',
         },
         {
-          label: "Пароль (ещё раз)",
-          inputType: "password",
-          inputValue: "password1",
+          label: 'Пароль (ещё раз)',
+          inputType: 'password',
+          inputValue: 'password1',
           invalid: true,
           showError: true,
-          errorMessage: "Пароли не совпадают",
-          name: "password_repeat",
+          errorMessage: 'Пароли не совпадают',
+          name: 'password_repeat',
         },
       ],
     },
@@ -82,102 +82,111 @@ const pages = {
     {
       infoRows: [
         {
-          label: "Почта",
-          value: "pochta@yandex.ru",
+          label: 'Почта',
+          value: 'pochta@yandex.ru',
         },
         {
-          label: "Логин",
-          value: "ivanivanov",
+          label: 'Логин',
+          value: 'ivanivanov',
         },
         {
-          label: "Имя",
-          value: "Иван",
+          label: 'Имя',
+          value: 'Иван',
         },
         {
-          label: "Фамилия",
-          value: "Иванов",
+          label: 'Фамилия',
+          value: 'Иванов',
         },
         {
-          label: "Имя в чате",
-          value: "Иван",
+          label: 'Имя в чате',
+          value: 'Иван',
         },
         {
-          label: "Телефон",
-          value: "+7 (909) 967 30 30",
+          label: 'Телефон',
+          value: '+7 (909) 967 30 30',
         },
       ],
-      profileName: "Иван",
+      profileName: 'Иван',
     },
   ],
-  profileEdit: [Pages.ProfileEditPage, {
-    editFields: [
-      {
-          label: "Почта",
-          inputType: "email",
-          inputValue: "pochta@yandex.ru",
-          name: "email",
+  profileEdit: [
+    Pages.ProfileEditPage,
+    {
+      editFields: [
+        {
+          label: 'Почта',
+          inputType: 'email',
+          inputValue: 'pochta@yandex.ru',
+          name: 'email',
         },
         {
-          label: "Логин",
-          inputType: "text",
-          inputValue: "ivanivanov",
-          name: "login",
+          label: 'Логин',
+          inputType: 'text',
+          inputValue: 'ivanivanov',
+          name: 'login',
         },
         {
-          label: "Имя",
-          inputType: "text",
-          inputValue: "Иван",
-          name: "first_name",
+          label: 'Имя',
+          inputType: 'text',
+          inputValue: 'Иван',
+          name: 'first_name',
         },
         {
-          label: "Фамилия",
-          inputType: "text",
-          inputValue: "Иванов",
-          name: "second_name",
+          label: 'Фамилия',
+          inputType: 'text',
+          inputValue: 'Иванов',
+          name: 'second_name',
         },
         {
-          label: "Имя в чате",
-          inputType: "text",
-          inputValue: "Иван",
-          name: "display_name",
+          label: 'Имя в чате',
+          inputType: 'text',
+          inputValue: 'Иван',
+          name: 'display_name',
         },
         {
-          label: "Телефон",
-          inputType: "tel",
-          inputValue: "+7 (909) 967 30 30",
-          name: "phone",
+          label: 'Телефон',
+          inputType: 'tel',
+          inputValue: '+7 (909) 967 30 30',
+          name: 'phone',
         },
-    ],
-    primaryButtonText: 'Сохранить',
-  }],
-   profileEditPassword: [Pages.ProfileEditPage, {
-    editFields: [
-      {
-        label: "Старый пароль",
-        inputType: "password",
-        inputValue: "oldPassword",
-        name: "password_old",
-      },
-      {
-        label: "Пароль",
-        inputType: "password",
-        inputValue: "password-new-222",
-        name: "newPassword",
-      },
-      {
-        label: "Пароль (ещё раз)",
-        inputType: "password",
-        inputValue: "password-new-222",
-        name: "newPasswordRepeat",
-      },
-    ],
-    primaryButtonText: 'Сохранить',
-  }],
-  "404": [Pages.Page404],
-  "500": [Pages.Page500],
-  modalPage: [Pages.PageWithModal, {
-    showModal: true,
-  }],
+      ],
+      primaryButtonText: 'Сохранить',
+    },
+  ],
+  profileEditPassword: [
+    Pages.ProfileEditPage,
+    {
+      editFields: [
+        {
+          label: 'Старый пароль',
+          inputType: 'password',
+          inputValue: 'oldPassword',
+          name: 'password_old',
+        },
+        {
+          label: 'Пароль',
+          inputType: 'password',
+          inputValue: 'password-new-222',
+          name: 'newPassword',
+        },
+        {
+          label: 'Пароль (ещё раз)',
+          inputType: 'password',
+          inputValue: 'password-new-222',
+          name: 'newPasswordRepeat',
+        },
+      ],
+      primaryButtonText: 'Сохранить',
+    },
+  ],
+  '404': [Pages.Page404],
+  '500': [Pages.Page500],
+  modalPage: [
+    Pages.PageWithModal,
+    {
+      showModal: true,
+    },
+  ],
   nav: [Pages.NavigatePage],
 };
 
@@ -188,19 +197,19 @@ Object.entries(Components).forEach(([name, template]) => {
 function navigate(page: string) {
   // @ts-ignore
   const [source, context] = pages[page];
-  const container = document.getElementById("app")!;
+  const container = document.getElementById('app')!;
 
   const templatingFunction = Handlebars.compile(source);
   container.innerHTML = templatingFunction(context);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  navigate("nav");
+document.addEventListener('DOMContentLoaded', () => {
+  navigate('main');
 });
 
-document.addEventListener("click", (e) => {
+document.addEventListener('click', e => {
   // @ts-ignore
-  const page = e.target.getAttribute("page");
+  const page = e.target.getAttribute('page');
   if (page) {
     navigate(page);
 
