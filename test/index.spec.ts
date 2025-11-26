1150c7ba68d00a1bc4c56ea1e7b316687f6b21bd
knatkojee <egor.v.kalmykov@gmail.com>
2025-11-26 16:09:37 +0300

    feat: added husky

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,10 @@
-import { expect } from "chai";
+// import { expect } from "chai";
 
-describe('Проверяем переходы у Роута', () => {
-  it('Переход на новую страницу должен менять состояние сущности history', () => {
-    // window.history.pushState({page: 'login'}, 'Login', '/login');
-    // window.history.pushState({page: 'register'}, 'Register', '/register');
+// describe('Проверяем переходы у Роута', () => {
+//   it('Переход на новую страницу должен менять состояние сущности history', () => {
+//     window.history.pushState({page: 'login'}, 'Login', '/login');
+//     window.history.pushState({page: 'register'}, 'Register', '/register');
 
-    // expect(window.history.length).to.eq(3);
-  });
-});
+//     expect(window.history.length).to.eq(3);
+//   });
+// });
