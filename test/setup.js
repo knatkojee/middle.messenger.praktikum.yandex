fe90462f1b527dc125a42e32f68e3d40950e2bb7
knatkojee <egor.v.kalmykov@gmail.com>
2025-11-27 15:21:05 +0300

    fix: tests setup fix

--- test/setup.js
+++ test/setup.js
@@ -8,4 +8,7 @@ const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
 
 global.window = dom.window;
 global.document = dom.window.document;
-global.navigator = dom.window.navigator;
+Object.defineProperty(global, 'navigator', {
+  value: dom.window.navigator,
+  writable: true
+});
